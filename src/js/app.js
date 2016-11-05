a26bda1acb8b85dcca8ec479ee0fb331379b0c84
Vedran Jaic <vedranjaic@icloud.com>
2016-11-05 19:06:57 +0100

    Template with two themes, toolbar, main menu, panels and tables

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,31 @@
+// --- [ THEME SWITCHER ]
+$('#theme-dark').click(function() {
+	$('body').removeClass('theme-light').addClass('theme-dark');
+});
+$('#theme-light').click(function() {
+	$('body').removeClass('theme-dark').addClass('theme-light');
+});
+
+
+// --- [ TOOLBAR ]
+// Toggle .hidden sidebar button
+$('.navbar-toggle').click(function() {
+	$(this).toggleClass('hidden');
+	$(this).siblings('.navbar-toggle').toggleClass('hidden');
+	$('body').toggleClass('main-menu-closed');
+});
+
+// Search
+$('.btn-search').click(function() {
+	$(this).siblings('.search-bar').toggleClass('search-collapsed');
+	$('#search-input').focus();
+});
+// Search reset
+$('.btn-search-reset').click(function() {
+	$('#search-input').val("").focus();
+})
+
+// --- [ MENU ]
 // Toggle .active on current .main-menu-item
 $('.main-menu-item > a').click(function() {
 	$('.main-menu-item').removeClass('active');
@@ -9,9 +37,17 @@ $('.submenu-item a').click(function() {
 	$(this).parent('.submenu-item').addClass('active');
 });
 
-// Toggle .hidden sidebar button
-$('.navbar-toggle').click(function() {
-	$(this).toggleClass('hidden');
-	$(this).siblings('.navbar-toggle').toggleClass('hidden');
-	$('body').toggleClass('main-menu-closed');
+
+//
+// --- [ PRESENTATION ] --- //
+//
+// Table click row shows hidden rows
+$('.table-row-l0').click(function() {
+	$(this).siblings('.table-row-l1').toggleClass('hidden');
+});
+$('.table-row-l1').click(function() {
+	$(this).siblings('.table-row-l2').toggleClass('hidden');
+});
+$('.table-row-l2').click(function() {
+	window.location.href = '/rules.html';
 });
